1d4cf6a919f4cabd7414b739cdbcce54005dacf8
Felipe Sousa <felipe.fps09@hotmail.com>
2022-11-01 21:32:36 -0300

    Initialized project

---
+++ backend/src/main/java/com/felipesousa/productcatalog/ProductcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.felipesousa.productcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/felipesousa/productcatalog/ProductcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipesousa.productcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
